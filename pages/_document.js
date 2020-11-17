43a0c5a1906ab545db58e12de0909a205a3eebdd
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-11-17 18:23:12 +0000

    Add SSR

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="fr">
@@ -10,6 +19,7 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/images/logo.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
