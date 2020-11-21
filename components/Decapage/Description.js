be0ad5f96f7a2bc079db2091a51fd8112c702a04
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-11-21 17:57:53 +0000

    Refactor traitement page

--- components/Decapage/Description.js
+++ components/Decapage/Description.js
@@ -1,64 +1,67 @@
-import styled from 'styled-components';
 import Link from 'next/link';
+import { WhiteDiv, DarkDiv } from '@/elements/Divisions';
+import Figure from '@/elements/IconFigure';
+import { IconsDiv, ServicesContainer } from '@/elements/FlexDiv';
 import Services from '@/components/Services';
-import ServicesContainer from '@/components/ServicesContainer';
 
-const Section = styled.section`
-  margin: auto;
-  margin-bottom: 50px;
-  & > p {
-    margin: auto;
-  }
-  & > ul {
-    margin: auto;
-    max-width: 800px;
-  }
-  & > p > a {
-    border-bottom: 1px solid black;
-  }
-  & > p:last-of-type {
-    margin: 30px auto;
-    text-align: center;
-  }
-`;
-
-const DarkDiv = styled.div`
-  background-color: #e1e1e1;
-  margin: 40px 0 20px 0;
-  padding: 20px 0 40px 0;
-  & > * {
-    margin: auto;
-  }
-`;
 
 const Description = () => {
   return (
-    <Section>
-      <h2>Les objectifs du décapage</h2>
-      <p>Nous redonnons aux ouvrages en métal (fer, acier, inox, cuivre, aluminium, etc...) leur <strong>aspect d’origine</strong>. Pour ce faire, nous enlevons la couche de matière qui s’est déposée à leur surface. Grâce à notre <strong>polyvalence</strong>, nous intervenons sur différents types de supports :</p>
-      <ul>
-        <li>façades</li>
-        <li>carrelages et meubles métalliques</li>
-        <li>carrosserie automobile</li>
-        <li>jantes</li>
-        <li>portails</li>
-        <li>garde-corps</li>
-        <li>volets</li>
-      </ul>
+    <section>
+      <WhiteDiv>
+        <h2>Les objectifs du décapage</h2>
+        <p>Nous redonnons aux ouvrages en métal (fer, acier, inox, cuivre, aluminium, etc...) leur <strong>aspect d’origine</strong>. Pour ce faire, nous enlevons la couche de matière qui s’est déposée à leur surface. Grâce à notre <strong>polyvalence</strong>, nous intervenons sur différents types de supports :</p>
+        <IconsDiv>
+          <Figure
+            name="Façades"
+            url="/images/icons/facade.webp"
+          />
+          <Figure
+            name="Meubles"
+            url="/images/icons/meuble.webp"
+          />
+          <Figure
+            name="Carrelages"
+            url="/images/icons/carrelage.webp"
+          />
+          <Figure
+            name="Carrosserie"
+            url="/images/icons/carrosserie.webp"
+          />
+          <Figure
+            name="Jantes"
+            url="/images/icons/jante.webp"
+          />
+          <Figure
+            name="Portails"
+            url="/images/icons/portail.webp"
+          />
+          <Figure
+            name="Garde-corps"
+            url="/images/icons/garde-corps.webp"
+          />
+          <Figure
+            name="Volets"
+            url="/images/icons/volet.webp"
+          />
+        </IconsDiv>
+      </WhiteDiv>
       <DarkDiv>
         <h2>Une technique innovante</h2>
         <p>Nous utilisons des techniques de décapage innovantes afin de redonner aux pièces métalliques, leur apparence d’antan. Les abrasifs que nous utilisons sont <strong>efficaces</strong> et <strong>n’endommagent pas</strong> la surface des ouvrages décapés.</p><br />
         <p><strong>Depuis 1945</strong>, nous avons pris en main de nombreux projets de décapage pour le compte des professionnels, des particuliers, des industries, des collectivités et des organismes publics.</p><br />
       </DarkDiv>
-      <h2>Retrouvez aussi...</h2>
-      <ServicesContainer>
-        <Services bg="/images/card.webp" name="Thermolaquage" page="/thermolaquage" />
-        <Services bg="/images/fluid.webp" name="Peinture fluide" page="/peinture-fluide" />
-        <Services bg="/images/sablage.webp" name="Sablage" page="/sablage" />
-        <Services bg="/images/corrosion.webp" name="Traitement anti-corrosion" page="/traitement-anti-corrosion" />
-      </ServicesContainer>
-      <p>Vous n'êtes pas certain de savoir ce qui correspond le mieux à vos besoins ? N'hésitez pas à <Link href="/contact"><a>nous contacter</a></Link>, nous serons ravis de vous aider.</p>
-    </Section>
+      <WhiteDiv>
+        <h2>Retrouvez aussi...</h2>
+        <ServicesContainer>
+          <Services bg="/images/card.webp" name="Thermolaquage" page="/thermolaquage" />
+          <Services bg="/images/fluid.webp" name="Peinture fluide" page="/peinture-fluide" />
+          <Services bg="/images/sablage.webp" name="Sablage" page="/sablage" />
+          <Services bg="/images/corrosion.webp" name="Traitement anti-corrosion" page="/traitement-anti-corrosion" />
+        </ServicesContainer>
+        <p>Vous n'êtes pas certain de savoir ce qui correspond le mieux à vos besoins ? N'hésitez pas à <Link href="/contact"><a>nous contacter</a></Link>, nous serons ravis de vous aider.</p>
+      </WhiteDiv>
+    </section>
   );
 }
 
--- components/Traitement/Description.js
+++ components/Traitement/Description.js
@@ -1,61 +1,69 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+import { WhiteDiv, DarkDiv } from '@/elements/Divisions';
+import { IconsDiv, ServicesContainer } from '@/elements/FlexDiv';
+import Figure from '../elements/IconFigure';
 import Services from '@/components/Services';
-import ServicesContainer from '@/components/ServicesContainer';
 
-const Section = styled.section`
-  margin: auto;
-  margin-bottom: 50px;
-  & > p {
-    margin: auto;
-  }
-  & > p > a {
-    border-bottom: 1px solid black;
-  }
-  & > p:last-of-type {
-    margin: 30px auto;
-    text-align: center;
-  }
-`;
-
-const DarkDiv = styled.div`
-  background-color: #e1e1e1;
-  margin: 40px 0 20px 0;
-  padding: 20px 0 40px 0;
-  & > * {
-    margin: auto;
-  }
-  & > ul {
-    max-width: 800px;
+const SpecialIconsDiv = styled(IconsDiv)`
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+  & > figure {
+    align-items: center;
+    display: flex;
+    justify-content: flex-start;
+    width: 430px;
+    & > figcaption {
+      margin-left: 20px;
+    }
+    &:last-of-type > figcaption {
+      border-bottom: 1px solid black;
+    }
   }
 `;
 
 const Description = () => {
   return (
-    <Section>
-      <h2>Une protection efficace et durable contre la rouille</h2>
-      <p>Pour protéger les pièces métalliques <strong>contre la rouille</strong>, nous appliquons des <strong>enduits</strong> qui font office de <strong>barrière à l’oxydation</strong>. Ils sont très résistants aux agressions climatiques et peuvent être appliqués <strong>sur tous types de supports</strong> en fer, en acier et en cuivre (pièces automobiles, ouvrages industriels, matériaux de construction, etc...).</p>
+    <section>
+      <WhiteDiv>
+        <h2>Une protection efficace et durable contre la rouille</h2>
+        <p>Pour protéger les pièces métalliques <strong>contre la rouille</strong>, nous appliquons des <strong>enduits</strong> qui font office de <strong>barrière à l’oxydation</strong>. Ils sont très résistants aux agressions climatiques et peuvent être appliqués <strong>sur tous types de supports</strong> en fer, en acier et en cuivre (pièces automobiles, ouvrages industriels, matériaux de construction, etc...).</p>
+      </WhiteDiv>
       <DarkDiv>
         <h2>Étapes du traitement anti-corrosion</h2>
         <p>Nous <strong>maîtrisons parfaitement</strong> les différentes étapes du traitement anti-corrosion des métaux :</p>
-        <ul>
-          <li>Nettoyage et préparation du support</li>
-          <li>Application du produit curatif contre la rouille</li>
-          <li>Application d’une couche de produit isolant</li>
-          <li><Link href="/thermolaquage"><a title="Découvrez nos prestations de thermolaquage">Thermolaquage</a></Link></li>
-        </ul><br />
+        <SpecialIconsDiv>
+          <Figure 
+            name="Nettoyage et préparation du support"
+            url="/images/icons/traitement/nettoyage.webp"
+          />
+          <Figure 
+            name="Application du produit curatif contre la rouille"
+            url="/images/icons/traitement/rouille.webp"
+          />
+          <Figure 
+            name="Application d’une couche de produit isolant"
+            url="/images/icons/traitement/isolation.webp"
+          />
+          <figure>
+            <img src="/images/icons/traitement/thermolaquage.webp" alt="Thermolaquage" />
+            <figcaption><em><Link href="/thermolaquage"><a title="Découvrez nos prestations de thermolaquage">Thermolaquage</a></Link></em></figcaption>
+          </figure>
+        </SpecialIconsDiv>
         <p>Quelles que soient la forme, les dimensions et les caractéristiques de la pièce métallique à traiter, nous n’avons qu’un seul objectif : <strong>enlever toutes les parties rouillées</strong> qui ternissent son état et son aspect.</p><br />
       </DarkDiv>
-      
-      <h2>Retrouvez aussi...</h2>
-      <ServicesContainer>
-        <Services bg="/images/card.webp" name="Thermolaquage" page="/thermolaquage" />
-        <Services bg="/images/fluid.webp" name="Peinture fluide" page="/peinture-fluide" />
-        <Services bg="/images/sablage.webp" name="Sablage" page="/sablage" />
-        <Services bg="/images/chem-pickling.webp" name="Décapage" page="/decapage" />
-      </ServicesContainer>
-      <p>Vous n'êtes pas certain de savoir ce qui correspond le mieux à vos besoins ? N'hésitez pas à <Link href="/contact"><a>nous contacter</a></Link>, nous serons ravis de vous aider.</p>
-    </Section>
+      <WhiteDiv>
+        <h2>Retrouvez aussi...</h2>
+        <ServicesContainer>
+          <Services bg="/images/card.webp" name="Thermolaquage" page="/thermolaquage" />
+          <Services bg="/images/fluid.webp" name="Peinture fluide" page="/peinture-fluide" />
+          <Services bg="/images/sablage.webp" name="Sablage" page="/sablage" />
+          <Services bg="/images/chem-pickling.webp" name="Décapage" page="/decapage" />
+        </ServicesContainer>
+        <p>Vous n'êtes pas certain de savoir ce qui correspond le mieux à vos besoins ? N'hésitez pas à <Link href="/contact"><a>nous contacter</a></Link>, nous serons ravis de vous aider.</p>
+      </WhiteDiv>
+    </section>
   );
 }
 
