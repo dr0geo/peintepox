264b25624227d045491f830627c638c13af3fc35
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-11-21 18:34:09 +0000

    Refactor success page

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { WhiteDiv } from '@/elements/Divisions';
+import { ServicesContainer } from '@/elements/FlexDiv';
 import Header from '@/components/Header';
 import PageTitle from '@/components/PageTitle';
 import ContactInfo from '@/components/ContactInfo';
@@ -10,20 +12,6 @@ import Services from '@/components/Services';
 const StyledAnchor = styled.a`
   border-bottom: 1px solid white;
   color: white;
-`;
-
-const StyledSC = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  margin: auto;
-  max-width: 1300px;
-  & a {
-    border-bottom: none;
-  }
-  @media only screen and (max-width: 1350px) {
-    max-width: 900px;
-  }
 `;
 
 const Success = props => {
@@ -42,34 +30,36 @@ const Success = props => {
           <h1>Message envoyé</h1>
           <p>Votre message a bien été envoyé et nous faisons tout notre possible pour vous répondre au plus vite.<br/>Vous pouvez maintenant revenir à la <Link href="/"><StyledAnchor>page d'accueil</StyledAnchor></Link>.</p>
         </PageTitle>
-        <h2>Retrouvez aussi...</h2>
-        <StyledSC>
-          <Services 
-            bg="/images/card.webp" 
-            name="Thermolaquage" 
-            page="/thermolaquage" 
-          />
-          <Services 
-            bg="/images/fluid.webp" 
-            name="Peinture fluide" 
-            page="/peinture-fluide"
-          />
-          <Services 
-            bg="/images/sablage.webp" 
-            name="Sablage" 
-            page="/sablage"
-          />
-          <Services 
-            bg="/images/chem-pickling.webp" 
-            name="Décapage" 
-            page="/decapage"
-          />
-          <Services 
-            bg="/images/corrosion.webp" 
-            name="Traitement anti-corrosion" 
-            page="/traitement-anti-corrosion"
-          />
-        </StyledSC>
+        <WhiteDiv>
+          <h2>Retrouvez aussi...</h2>
+          <ServicesContainer>
+            <Services 
+              bg="/images/card.webp" 
+              name="Thermolaquage" 
+              page="/thermolaquage" 
+            />
+            <Services 
+              bg="/images/fluid.webp" 
+              name="Peinture fluide" 
+              page="/peinture-fluide"
+            />
+            <Services 
+              bg="/images/sablage.webp" 
+              name="Sablage" 
+              page="/sablage"
+            />
+            <Services 
+              bg="/images/chem-pickling.webp" 
+              name="Décapage" 
+              page="/decapage"
+            />
+            <Services 
+              bg="/images/corrosion.webp" 
+              name="Traitement anti-corrosion" 
+              page="/traitement-anti-corrosion"
+            />
+          </ServicesContainer>
+        </WhiteDiv>
         <ContactInfo />
       </main>
       <Footer />
