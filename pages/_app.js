75c500efc4dc0d956684d44a12003f185daefd46
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-11-23 12:37:31 +0000

    Use font-face

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,33 @@ import { createGlobalStyle } from 'styled-components';
 import { useState } from 'react';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-display: swap;
+    font-family: 'Tajawal';
+    font-weight: 400;
+    src: 
+      url("/fonts/Tajawal-Regular.woff2") format("woff2"),
+      url("/fonts/Tajawal-Regular.woff") format("woff");
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Tajawal';
+    font-weight: 300;
+    src: 
+      url("/fonts/Tajawal-Light.woff2") format("woff2"),
+      url("/fonts/Tajawal-Light.woff") format("woff");
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Tajawal';
+    font-weight: 700;
+    src: 
+      url("/fonts/Tajawal-Bold.woff2") format("woff2"),
+      url("/fonts/Tajawal-Bold.woff") format("woff");
+  }
+
   html {
     font-size: 16px;
   }
